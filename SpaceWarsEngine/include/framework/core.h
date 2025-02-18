d475712361113f6e7697455ec8e03a73e8ce287d
Pierce Provias <p.provias@outlook.com>
2025-02-18 09:05:32 -0500

    Defined common data types

--- SpaceWarsEngine/include/framework/core.h
+++ SpaceWarsEngine/include/framework/core.h
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <memory>
+#include <vector>
+#include <map>
+#include <unordered_map>
 
 namespace sw
 {
     #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
+
+    template<typename T>
+    using unique = std::unique_ptr<T>;
+
+    template<typename T>
+    using shared = std::shared_ptr<T>;
+
+    template<typename T>
+    using weak = std::weak_ptr<T>;
+
+    template<typename T>
+    using list = std::vector<T>;
+
+    template <typename key_type, typename value_type, typename pr = std::less<key_type>>
+    using map = std::map<key_type, value_type, pr>;
+
+    template <typename key_type, typename value_type, typename hasher = std::hash<key_type>>
+    using dictionary = std::unordered_map<key_type, value_type, hasher>;
 }
